73587d6de27017dd0046d063ebe2bbe76995818b
Timur Valeev <timurrr@gmail.com>
2022-06-29 23:20:52 +0500

    added new game - even and recored asciinema, see READMI.md

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,14 +6,17 @@ public class App {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.\n" +
-                    "1 - Greet\n" +
-                    "0 - Exit\n");
+                "1 - Greet\n" +
+                "2 - Even\n" +
+                "0 - Exit\n");
         System.out.print("Your choice: ");
         if(num.hasNextInt()) {
             int inputNumber = num.nextInt();
             if(inputNumber == 1) {
-                System.out.println("Welcome to the Brain Games!");
                 Cli.getName();
+            }
+            if(inputNumber == 2) {
+                Even.play();
             }
        }
     }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -6,11 +6,7 @@ public class Cli {
     public static void getName() {
         System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
-        //if добавлен. чтобы избавиться от ошибки
-        //java.util.NoSuchElementException: No line found...
-        if(sc.hasNextLine()) {
-            String name = sc.nextLine();
-            System.out.println("Hello, " + name + "!");
-        }
+        String name = sc.next();
+        System.out.println("Hello, " + name + "!");
     }
 }
