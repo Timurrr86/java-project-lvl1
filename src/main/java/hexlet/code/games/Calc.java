7f79466475be4f81d6a7821445b9c449372a2c96
Timur Valeev <timurrr@gmail.com>
2022-07-08 00:17:20 +0500

    refactored Calc game

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -3,38 +3,23 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Calc {
+    private static final String task = "What is the result of the expression?\n";
+    private static final String[] questions = new String[3];
+    private static final String[] correctAnswer = new String[3];
+
     public static void play() {
-        String name = Engine.getPlayerName();
-        System.out.println("Hello, " + name + "!");
-        System.out.println("What is the result of the expression?\n");
-        int numberOfCorrectAnswer = 0;
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             String[] operators = {"+", "-", "*"};
+            int randomNumber1 = Engine.getRandomNumber();
+            int randomNumber2 = Engine.getRandomNumber();
             int count = (int) (Math.random() * 2);
-            //выводим число в диапазоне от 1 до 100
-            int x = 1 + (int) (Math.random() * 100);
-            int y = 1 + (int) (Math.random() * 100);
-            System.out.println("Question: " + x + operators[count] + y);
-            int answer = Engine.getAnswerInt();
-            int result = 0;
+            questions[i] = String.format("%s %s %s", randomNumber1, operators[count], randomNumber2);
             switch (operators[count]) {
-                case "+" -> result = x + y;
-                case "-" -> result = x - y;
-                case "*" -> result = x * y;
+                case "+" -> correctAnswer[i] = String.valueOf(randomNumber1 + randomNumber2);
+                case "-" -> correctAnswer[i] = String.valueOf(randomNumber1 - randomNumber2);
+                case "*" -> correctAnswer[i] = String.valueOf(randomNumber1 * randomNumber2);
             }
-            if(answer == result) {
-                System.out.println("Correct!");
-                numberOfCorrectAnswer += 1;
-                } else {
-                    System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was" + "'" + result + "'" + ".\n" +
-                            "Let's try again, " + name);
-                    break;
-                }
-            System.out.println();
-
         }
-        if(numberOfCorrectAnswer >= 3) {
-            System.out.println("Congratulations, " + name);
-        }    }
-
+        Engine.gameBody(task, questions, correctAnswer);
+    }
 }
