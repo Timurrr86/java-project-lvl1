9d6d09d9cb0d8c22fd6d06ec8eaa841dea355f46
Timur Valeev <timurrr@gmail.com>
2022-07-13 16:27:57 +0500

    created new makeCalculation method, fixed count generation for operators

--- src/main/java/hexlet/code/Utils.java
+++ src/main/java/hexlet/code/Utils.java
@@ -2,8 +2,8 @@ package hexlet.code;
 
 public class Utils {
 
-    public static int getRandomNumber(int MIN_RANDOM_NUMBER, int MAX_RANDOM_NUMBER) {
+    public static int getRandomNumber(int minRandomNumber, int maxRandomNumber) {
         //формируем число от 1 до 100
-        return MIN_RANDOM_NUMBER + (int) (Math.random() * MAX_RANDOM_NUMBER);
+        return minRandomNumber + (int) (Math.random() * maxRandomNumber);
     }
 }
--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -9,30 +9,35 @@ public class Calc {
     private static final String[] CORRECTANSWER = new String[Engine.NUMBEROFROUNDS];
     private static final int MIN_RANDOM_NUMBER = 1;
     private static final int MAX_RANDOM_NUMBER = 100;
+    private static final String[] OPERATORS = {"+", "-", "*"};
 
     public static void play() {
         for (int i = 0; i < Engine.NUMBEROFROUNDS; i++) {
-            String[] operators = {"+", "-", "*"};
             int randomNumber1 = Utils.getRandomNumber(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
             int randomNumber2 = Utils.getRandomNumber(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
-            int count = (int) (Math.random() * 2);
-            QUESTIONS[i] = String.format("%s %s %s", randomNumber1, operators[count], randomNumber2);
-            int resultOfOperation = 0;
-            switch (operators[count]) {
-                case "+":
-                    resultOfOperation = randomNumber1 + randomNumber2;
-                    break;
-                case "-":
-                    resultOfOperation = randomNumber1 - randomNumber2;
-                    break;
-                case "*":
-                    resultOfOperation = randomNumber1 * randomNumber2;
-                    break;
-                default:
-                    break;
-            }
-            CORRECTANSWER[i] = String.valueOf(resultOfOperation);
+            int count = Utils.getRandomNumber(0, OPERATORS.length - 1);
+            QUESTIONS[i] = String.format("%s %s %s", randomNumber1, OPERATORS[count], randomNumber2);
+            CORRECTANSWER[i] = makeCalculation(randomNumber1, randomNumber2, OPERATORS[count]);
+
         }
         Engine.runGame(TASK, QUESTIONS, CORRECTANSWER);
+    }
+
+    public static String makeCalculation(int randomNumber1, int randomNumber2, String operator) {
+        int resultOfOperation = 0;
+        switch (operator) {
+            case "+":
+                resultOfOperation = randomNumber1 + randomNumber2;
+                break;
+            case "-":
+                resultOfOperation = randomNumber1 - randomNumber2;
+                break;
+            case "*":
+                resultOfOperation = randomNumber1 * randomNumber2;
+                break;
+            default:
+                break;
+        }
+        return String.valueOf(resultOfOperation);
     }
 }
